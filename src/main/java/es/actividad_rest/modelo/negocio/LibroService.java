94f9bc11b1509c8bbed2e39709d0d63e42a5da1c
sergiopalmero <sergiopalmero87@gmail.com>
2023-11-11 14:25:02 +0100

    PutMapping

--- src/main/java/es/actividad_rest/modelo/negocio/LibroService.java
+++ src/main/java/es/actividad_rest/modelo/negocio/LibroService.java
@@ -16,11 +16,11 @@ public class LibroService {
 	public LibroService() {
 		System.out.println("Logica -> Creando la lista de libros!");
 		listaLibros = new ArrayList<Libro>();
-		Libro l1 = new Libro(0, "Libro 1", "Editorial 1", 10.0);
-		Libro l2 = new Libro(1, "Libro 2", "Editorial 2", 12.0);
-		Libro l3 = new Libro(2, "Libro 3", "Editorial 3", 14.0);
-		Libro l4 = new Libro(3, "Libro 4", "Editorial 4", 16.0);
-		Libro l5 = new Libro(4, "Libro 5", "Editorial 5", 18.0);
+		Libro l1 = new Libro(0, "Libro 0", "Editorial 0", 10.0);
+		Libro l2 = new Libro(1, "Libro 1", "Editorial 1", 12.0);
+		Libro l3 = new Libro(2, "Libro 2", "Editorial 2", 14.0);
+		Libro l4 = new Libro(3, "Libro 3", "Editorial 3", 16.0);
+		Libro l5 = new Libro(4, "Libro 4", "Editorial 4", 18.0);
 		listaLibros.add(l1);
 		listaLibros.add(l2);
 		listaLibros.add(l3);
@@ -54,20 +54,17 @@ public class LibroService {
 	
 	
 	//Update
-	public Libro editarLibro(Libro libro) {
-		
-		try {
-		Libro libroAux = listaLibros.get(libro.getId());
-		libroAux.setTitulo(libro.getTitulo());
-		libroAux.setEditorial(libro.getEditorial());
-		libroAux.setNota(libro.getNota());
-
-		return libroAux;
-		
-		} catch (IndexOutOfBoundsException iobe) {
-			System.out.println("update -> Libro fuera de rango");
-			return null;
+	public Libro editarLibro(Libro libroNuevo) {
+		for(Libro libro : listaLibros) {
+			if(libroNuevo.getId() == libro.getId()) {
+				libro.setId(libroNuevo.getId());
+				libro.setTitulo(libroNuevo.getTitulo());
+				libro.setEditorial(libroNuevo.getEditorial());
+				libro.setNota(libroNuevo.getNota());
+				return libroNuevo;
+			}
 		}
+		return null;
 		
 	}
 	

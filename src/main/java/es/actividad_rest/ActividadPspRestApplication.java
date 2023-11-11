011b42ab856bf48e21de11635e660fdffef2a73a
sergiopalmero <sergiopalmero87@gmail.com>
2023-11-11 12:15:11 +0100

    Creada clase LibroControlador

--- src/main/java/es/actividad_rest/ActividadPspRestApplication.java
+++ src/main/java/es/actividad_rest/ActividadPspRestApplication.java
@@ -15,7 +15,7 @@ import es.actividad_rest.controlador.LibroControlador;
 import es.actividad_rest.modelo.negocio.LibroService;
 
 @SpringBootApplication
-public class ActividadPspRestApplication implements CommandLineRunner{
+public class ActividadPspRestApplication {
 	
 	@Autowired
 	private LibroService libroService;
@@ -38,57 +38,7 @@ public class ActividadPspRestApplication implements CommandLineRunner{
 		SpringApplication.run(ActividadPspRestApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Scanner scanner = new Scanner(System.in);
-		int opcion;
-        do {
-            mostrarMenu();
-            opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumir la nueva línea después de leer el número
-
-            switch (opcion) {
-                case 1:
-                    // Dar de alta un libro
-                    // ...
-                    break;
-                case 2:
-                    // Dar de baja un libro por ID
-                    // ...
-                    break;
-                case 3:
-                    // Modificar un libro por ID
-                    // ...
-                    break;
-                case 4:
-                    // Obtener un libro por ID
-                    // ...
-                    break;
-                case 5:
-                    // Listar todos los libros
-                    // ...
-                    break;
-                case 6:
-                    System.out.println("Saliendo de la aplicación.");
-                    break;
-                default:
-                    System.out.println("Opción no válida. Inténtalo de nuevo.");
-                    break;
-            }
-
-        } while (opcion != 6);
-    }
-
-    private static void mostrarMenu() {
-        System.out.println("Menú:");
-        System.out.println("1. Dar de alta un libro");
-        System.out.println("2. Dar de baja un libro por ID");
-        System.out.println("3. Modificar un libro por ID");
-        System.out.println("4. Obtener un libro por ID");
-        System.out.println("5. Listar todos los libros");
-        System.out.println("6. Salir");
-        System.out.print("Selecciona una opción: ");
-    }
+	
 
 		
 }
--- src/main/java/es/actividad_rest/controlador/LibroControlador.java
+++ src/main/java/es/actividad_rest/controlador/LibroControlador.java
@@ -1,8 +1,88 @@
 package es.actividad_rest.controlador;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.actividad_rest.modelo.entidad.Libro;
+import es.actividad_rest.modelo.negocio.LibroService;
 
 @RestController
 public class LibroControlador {
-
+	
+	@Autowired
+	LibroService libroService;
+	
+	
+	//Obtener libro por ID
+	@GetMapping(path="libros/{id}",produces = MediaType.APPLICATION_JSON_VALUE)	
+	public ResponseEntity<Libro> obtenerLibroPorId(@PathVariable("id") int id) {
+		System.out.println("Buscando libro con id: " + id);
+		Libro libro = libroService.obtenerLibroPorId(id);
+		if(libro != null) {
+			return new ResponseEntity<Libro>(libro,HttpStatus.OK);//200 OK
+		}else {
+			return new ResponseEntity<Libro>(HttpStatus.NOT_FOUND);//404 NOT FOUND
+		}
+	}
+	
+	//Obtener todos los libros
+	@GetMapping(path="libros",produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Libro>> obtenerTodosLosLibros(){
+		List<Libro> listaLibros;
+		System.out.println("Listando los libros");
+		listaLibros = libroService.obtenerTodosLosLibros();			
+		return new ResponseEntity<List<Libro>>(listaLibros,HttpStatus.OK);
+		}
+	
+	//Borrar un libro segun su id.
+	@DeleteMapping(path="libros/{id}")
+	public ResponseEntity<Libro> eliminarLibroPorId(@PathVariable("id") int id) {
+		System.out.println("Libro a borrar por su id: " + id);
+		Libro libro = libroService.eliminarLibroPorId(id);
+		if(libro != null) {
+			return new ResponseEntity<Libro>(libro,HttpStatus.OK);//200 OK
+		}else {
+			return new ResponseEntity<Libro>(HttpStatus.NOT_FOUND);//404 NOT FOUND
+		}
+	}
+	
+	//Dar de alta un libro
+	@PostMapping(path="libros",
+			consumes=MediaType.APPLICATION_JSON_VALUE,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Libro> altaLibro(@RequestBody Libro libro) {
+		System.out.println("altaLibro: objeto libro: " + libro);
+		libroService.altaLibro(libro);
+		return new ResponseEntity<Libro>(libro,HttpStatus.CREATED);//201 CREATED
+	}
+	
+	//Modificar libro segun su id
+	@PutMapping(path="libros/{id}",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Libro> editarLibro(@PathVariable("id") int id, @RequestBody Libro libro) {
+		System.out.println("libro a modificar segun su id: " + id);
+		System.out.println("Datos a modificar: " + libro);
+		Libro libroActualizado = libroService.editarLibro(libro);
+		if(libroActualizado != null) {
+			return new ResponseEntity<Libro>(libroActualizado, HttpStatus.OK);//200 OK
+		}else {
+			return new ResponseEntity<Libro>(HttpStatus.NOT_FOUND);//404 NOT FOUND
+		}
+	}
+	
 }
+		
+	
+
+

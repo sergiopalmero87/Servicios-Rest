b6b4899cfaf07aba48a47b1972a21f8936433805
sergiopalmero <sergiopalmero87@gmail.com>
2023-11-14 16:38:44 +0100

    Comentarios metodos RestTemplate

--- src/main/java/es/actividad_rest/ActividadPspRestApplication.java
+++ src/main/java/es/actividad_rest/ActividadPspRestApplication.java
@@ -12,12 +12,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import es.actividad_rest.controlador.LibroControlador;
+import es.actividad_rest.modelo.entidad.Libro;
 import es.actividad_rest.modelo.negocio.LibroService;
 
 @SpringBootApplication
-public class ActividadPspRestApplication {
+public class ActividadPspRestApplication { //implements CommandLineRunner {
 	
-	@Autowired
+	
 	private LibroService libroService;
 	
 	@Autowired
@@ -31,6 +32,11 @@ public class ActividadPspRestApplication {
 		return builder.build();
 	}
 	
+	@Autowired
+	public ActividadPspRestApplication(LibroService libroService) {
+	    this.libroService = libroService;
+	}
+
 	
 
 	public static void main(String[] args) {
@@ -38,8 +44,111 @@ public class ActividadPspRestApplication {
 		SpringApplication.run(ActividadPspRestApplication.class, args);
 	}
 
-	
 
+
+	/*public void menu() {
+		System.out.println("\n=== Menú ===");
+        System.out.println("1. Dar de alta un libro");
+        System.out.println("2. Eliminar un libro por ID");
+        System.out.println("3. Modificar un libro por ID");
+        System.out.println("4. Obtener un libro por ID");
+        System.out.println("5. Listar todos los libros");
+        System.out.println("6. Salir");
+	}*/
+
+
+
+	/*@Override
+	public void run(String... args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+		int opcion = 0;
+		
+		do {
+			menu();
+			System.out.print("Elige una opcion: \n");
+			opcion = scan.nextInt();
+			
+			switch(opcion) {
+				case 1: 
+					System.out.println("Introduce los datos de la nueva pelicula:");
+					System.out.print("ID: ");
+					int id = scan.nextInt();
+					scan.nextLine();
+					System.out.print("TITULO: ");
+					String titulo = scan.nextLine();
+					System.out.print("EDITORIAL: ");
+					String editorial = scan.nextLine();
+					System.out.print("NOTA");
+					double nota = scan.nextDouble();
+					Libro libro = new Libro(id,titulo, editorial, nota);
+					libroService.altaLibroRs(libro);
+					break;
+				case 2:
+					System.out.print("Introduce el ID del libro a eliminar: ");
+                    int idEliminar = scan.nextInt();
+                    libroService.eliminarLibroPorIdRs(idEliminar);
+                    break;
+				case 3:
+					System.out.println("Introduce los datos de la pelicula a editar:");
+					System.out.print("ID: ");
+                    int idEditar = scan.nextInt();
+                    scan.nextLine();  // Consumir el salto de línea pendiente
+                    System.out.print("TITULO: ");
+                    String nuevoTitulo = scan.nextLine();
+                    System.out.print("EDITORIAL: ");
+                    String nuevaEditorial = scan.nextLine();
+                    System.out.print("NOTA: ");
+                    double nuevaNota = scan.nextDouble();
+                    Libro libroEditado = new Libro(idEditar, nuevoTitulo, nuevaEditorial, nuevaNota);
+                    libroService.editarLibroRs(libroEditado);
+                    break;
+				case 4:
+					System.out.print("Introduce el ID del libro a consultar: ");
+                    int idConsultar = scan.nextInt();
+                    libroService.obtenerLibroPorIdRs(idConsultar);
+                    break;
+				case 5:
+					System.out.println("La lista de libros es: ");
+					libroService.obtenerTodosLosLibrosRs();
+					break;
+				case 6:
+					System.out.println("Saliendo de la aplicacion!");
+					pararAplicacion();
+					break;
+				default:
+					System.out.println("Opcion no valida. Intentelo de nuevo.");
+				
+			}
+		
+		}while(opcion != 6);
+		
+		
+	}
+	
+	
+	public void pararAplicacion() {
+		//Esta aplicacion levanta un servidor web, por lo que tenemos que dar 
+		//la orden de pararlo cuando acabemos. Para ello usamos el método exit, 
+		//de la clase SpringApplication, que necesita tanto el contexto de 
+		//Spring como un objeto que implemente la interfaz ExitCodeGenerator. 
+		//Podemos usar la función lambda "() -> 0" para simplificar 
+		
+		SpringApplication.exit(context, () -> 0);
+		
+		//Podemos hacerlo también de una manera clásica, es decir, creando
+		//la clase anónima a partir de la interfaz. 
+		//El código de abajo sería equivalente al de arriba
+		//(pero mucho más largo)
+		/*
+		SpringApplication.exit(context, new ExitCodeGenerator() {
+			
+			@Override
+			public int getExitCode() {
+				return 0;
+			}
+		});*/
+	
+	
 		
 }
 
--- src/main/java/es/actividad_rest/modelo/negocio/LibroService.java
+++ src/main/java/es/actividad_rest/modelo/negocio/LibroService.java
@@ -1,9 +1,16 @@
 package es.actividad_rest.modelo.negocio;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import es.actividad_rest.modelo.entidad.Libro;
 
@@ -12,9 +19,15 @@ public class LibroService {
 
 	public List<Libro> listaLibros;
 	public int contador;
+	
+	//La URL base del servicio REST de libros
+	public static final String URL = "http://localhost:8080/libros";
+	
+	/*@Autowired
+	RestTemplate restTemplate;*/
 
 	public LibroService() {
-		System.out.println("Logica -> Creando la lista de libros!");
+		System.out.println("LibroService -> Creando la lista de libros!");
 		listaLibros = new ArrayList<Libro>();
 		Libro l1 = new Libro(0, "Libro 0", "Editorial 0", 10.0);
 		Libro l2 = new Libro(1, "Libro 1", "Editorial 1", 12.0);
@@ -26,6 +39,8 @@ public class LibroService {
 		listaLibros.add(l3);
 		listaLibros.add(l4);
 		listaLibros.add(l5);
+		System.out.println("\n-----------------------------------------\n");
+		System.out.println("LibroService -> Creada lista de libros!");
 	}
 	
 	//Metodos CRUD.
@@ -34,12 +49,44 @@ public class LibroService {
         return listaLibros;
     }
 	
+	/*//Obtener libros con restTemplate
+	public List<Libro> obtenerTodosLosLibrosRs(){
+		try {
+			ResponseEntity<Libro[]> obtenerTodosLosLibros =
+					  restTemplate.getForEntity(URL,Libro[].class);
+			Libro[] arrayLibros = obtenerTodosLosLibros.getBody();
+			return Arrays.asList(arrayLibros);//convertimos el array en un ArrayList
+		} catch (HttpClientErrorException e) {
+			System.out.println("obtenerTodosLosLibrosRs -> Error al obtener la lista de libros");
+		    System.out.println("obtenerTodosLosLibrosRs -> Codigo de respuesta: " + e.getStatusCode());
+		    return null;
+		}
+	}*/
+	
 	
 	//Create
 	public List<Libro> altaLibro(Libro l) {
 		listaLibros.add(l);
 		return listaLibros;
 	}
+	
+	/*//Create con restTemplate
+	public Libro altaLibroRs(Libro libro){
+		try {
+			//Para hacer un post de una entidad usamos el metodo postForEntity
+			//El primer parametro la URL
+			//El segundo parametro la persona que ira en body
+			//El tercer parametro el objeto que esperamos que nos envie el servidor
+			ResponseEntity<Libro> re = restTemplate.postForEntity(URL, libro, Libro.class);
+			System.out.println("altaLibroRs -> Codigo de respuesta " + re.getStatusCode());
+			System.out.println("Libro dado de alta correctamente!");
+			return re.getBody();
+		} catch (HttpClientErrorException e) {//Errores 4XX
+			System.out.println("altaLibroRs -> El libro NO se ha dado de alta, id: " + libro);
+		    System.out.println("altaLibroRs -> Codigo de respuesta: " + e.getStatusCode());
+		    return null;
+		}
+	}*/
 	
 	
 	//Read
@@ -51,6 +98,27 @@ public class LibroService {
 	        }
 	        return null; // No se encontró ninguna película con ese ID
 	    }
+	
+	/*//Read con restTemplate
+	public Libro obtenerLibroPorIdRs(int id){
+		try {
+			//Como el servicio trabaja con objetos ResponseEntity, nosotros 
+			//tambien podemos hacerlo en el cliente
+			//Ej http://localhost:8080/personas/1 GET
+			ResponseEntity<Libro> re = restTemplate.getForEntity(URL + id, Libro.class);
+			HttpStatusCode hs= re.getStatusCode();
+			if(hs == HttpStatus.OK) {	
+				return re.getBody();
+			}else {
+				System.out.println("obtenerLibroPorId -> Respuesta no contemplada");
+				return null;
+			}
+		}catch (HttpClientErrorException e) {//Errores 4XX
+			System.out.println("obtenerLibroPorId -> La persona NO se ha encontrado, id: " + id);
+		    System.out.println("obtenerLibroPorId -> Codigo de respuesta: " + e.getStatusCode());
+		    return null;
+		}
+	}*/
 	
 	
 	//Update
@@ -68,24 +136,21 @@ public class LibroService {
 		
 	}
 	
-	/*//Update 2
-	public Libro editarLibro(int id) {
+	/*//Update con restTemplate
+	public boolean editarLibroRs(Libro libro){
 		try {
-			for(Libro libro : listaLibros) {
-				if(id == libro.getId()) {
-					Libro libroAux = null;
-					libro.setTitulo(libroAux.getTitulo());
-					libro.setEditorial(libroAux.getEditorial());
-					libro.setNota(libroAux.getNota());
-					return libroAux;
-				}
-			}
-			
-		} catch (Exception e) {
-			
+			//El metodo put de Spring no devuelve nada
+			//si no da error se ha dado de alta y si no daria una 
+			//excepcion
+			restTemplate.put(URL + libro.getId(), libro, Libro.class);
+			return true;
+		} catch (HttpClientErrorException e) {
+			System.out.println("editarLibroRs -> El libro no se ha modificado, id: " + libro.getId());
+		    System.out.println("editarLibroRs -> Codigo de respuesta: " + e.getStatusCode());
+		    return false;
 		}
-		return null;
 	}*/
+	
 	
 	//Delete
 	public Libro eliminarLibroPorId(int id) {
@@ -97,6 +162,22 @@ public class LibroService {
         }
         return null; // No se encontró ninguna película con ese ID
     }
+	
+	/*//Delete con restTemplate
+	public boolean eliminarLibroPorIdRs(int id){
+		try {
+			//El metodo delete tampoco devuelve nada, por lo que si no 
+			//ha podido borrar el id, daría un excepcion
+			//Ej http://localhost:8080/personas/1 DELETE
+			restTemplate.delete(URL + id);
+			System.out.println("Libro borrado correctamente!");
+			return true;
+		} catch (HttpClientErrorException e) {
+			System.out.println("eliminarLibroPorIdRs -> El libro NO se ha borrado, id: " + id);
+		    System.out.println("eliminarLibroPorIdRs -> Codigo de respuesta: " + e.getStatusCode());
+		    return false;
+		}
+	}*/
 	
 	
 	
